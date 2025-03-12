31588c88744f0fa3f04600ae0dac954baae88a3f
Maartz <maartz@protonmail.com>
2025-03-12 14:23:42 +0100

    dedicate functions to lean the init
    as most of the code is static, delegating its content to dedicated
    function is necessary

--- src/gabarit_sup.erl
+++ src/gabarit_sup.erl
@@ -6,6 +6,7 @@
 -behaviour(supervisor).
 -export([start_link/0]).
 -export([init/1]).
+-export([get_persistence_options/0, gabarit_store_spec/1]).
 -define(SERVER, ?MODULE).
 
 start_link() ->
@@ -15,23 +16,33 @@ init([]) ->
     SupFlags = #{strategy => one_for_all,
                  intensity => 0,
                  period => 1},
-    %% TODO: which option is better for the application
-    StoreOpts = case application:get_env(gabarit, persistence, none) of
-                    none -> [];
-                    dets ->
-                        BaseDir = application:get_env(gabarit, dets_dir, "priv/gabarit_store"),
-                        [{persistence_module, gabarit_dets_persistence}, {persistence_dir, BaseDir}];
-                    mnesia ->
-                        [{persistence_module, gabarit_mnesia_persistence}]
-                end,
-
-    ChildSpecs = [
-        #{id => gabarit_store,
-          start => {gabarit_store, start_link, [StoreOpts]},
-          restart => permanent,
-          shutdown => 5000,
-          type => worker,
-          modules => [gabarit_store]}
-    ],
-
+    StoreOpts = get_persistence_options(),
+    ChildSpecs = [gabarit_store_spec(StoreOpts)],
     {ok, {SupFlags, ChildSpecs}}.
+
+%% @doc Gets persistence options based on application configuration.
+%% @returns List of options to pass to the gabarit_store.
+-spec get_persistence_options() -> list().
+get_persistence_options() ->
+    case application:get_env(gabarit, persistence, none) of
+        none ->
+            [];
+        dets ->
+            BaseDir = application:get_env(gabarit, dets_dir, "priv/gabarit_store"),
+            [{persistence_module, gabarit_dets_persistence},
+             {persistence_dir, BaseDir}];
+        mnesia ->
+            [{persistence_module, gabarit_mnesia_persistence}]
+    end.
+
+%% @doc Creates a child spec for the gabarit_store process.
+%% @param StoreOpts Options to pass to the store when starting it.
+%% @returns A supervisor child specification.
+-spec gabarit_store_spec(list()) -> supervisor:child_spec().
+gabarit_store_spec(StoreOpts) ->
+    #{id => gabarit_store,
+      start => {gabarit_store, start_link, [StoreOpts]},
+      restart => permanent,
+      shutdown => 5000,
+      type => worker,
+      modules => [gabarit_store]}.
